4141869430eef05f7c8a418e73c84767d37e4cf6
Sam Teel <sjkteel@hotmail.com>
2023-07-29 12:45:46 -0400

    wrote associations in the models index file

--- models/index.js
+++ models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: null
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
+
+
+/* NEED HELP CHECKING THESE WORK CORRECT, DO THEY NEED FOREIGN KEYS ASWELL?*/
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, { through: 'ProductTag' });
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(ProductTag, { through: 'ProductTag' });
 module.exports = {
   Product,
   Category,
