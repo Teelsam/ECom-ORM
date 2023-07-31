00d81d49ae084be41d8aae7da164cbbe1e802986
Sam Teel <sjkteel@hotmail.com>
2023-07-31 19:25:37 -0400

    temp fix for last prod route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,15 +114,15 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const delProduct = await Product.destroy({
-      where: {
-        id: req.params.id,
-      }
-    });
-    if (!delProduct) {
-      res.status(404).json({ message: 'no matching product' });
-    }
-    res.status(200).json(delProduct);
+    // const delProduct = await Product.destroy({
+    //   where: {
+    //     id: req.params.id,
+    //   }
+    // });
+    // if (!delProduct) {
+    //   res.status(404).json({ message: 'no matching product' });
+    // }
+    res.status(200).json({ message: 'not currently functional, see databases' });
   } catch (err) {
     res.status(500).json(err);
   }
